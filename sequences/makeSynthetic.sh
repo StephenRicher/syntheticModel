d2e40c245af1b59c79af343431103d03cad07780
Stephen Richer <sr467@bath.ac.uk>
2020-10-03 20:50:27 +0100

    Add new sequences.

--- sequences/makeSynthetic.sh
+++ sequences/makeSynthetic.sh
@@ -1,10 +1,13 @@
-../../workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 409 --intraClusterDistance 30 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster30.txt
+../../pipeline/workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 409 --intraClusterDistance 30 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster30.txt
 
-../../workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 379 --intraClusterDistance 50 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster50.txt
+../../pipeline/workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 379 --intraClusterDistance 50 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster50.txt
 
-../../workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 349 --intraClusterDistance 70 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster70.txt
+../../pipeline/workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 349 --intraClusterDistance 70 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster70.txt
 
-../../workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 319 --intraClusterDistance 90 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster90.txt
+../../pipeline/workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 319 --intraClusterDistance 90 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster90.txt
 
-../../workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 289 --intraClusterDistance 110 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster110.txt
+../../pipeline/workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 289 --intraClusterDistance 110 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster110.txt
 
+../../pipeline/workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 205 --intraClusterDistance 130 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster130.txt
+
+../../pipeline/workflow/scripts/createSynthetic.py 3N3N3N3 --nClusters 3 --interClusterDistance 175 --intraClusterDistance 150 --intraClusterBeads 4 --buffer 25 --bead N > syntheticSequence-subCluster150.txt
